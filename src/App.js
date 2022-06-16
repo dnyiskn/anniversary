0a5781058fd3087aef33653b7c857c8e0d2e5a26
dnyiskn <dnyiskn@gmail.com>
2022-06-15 23:02:52 -0400

    added files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a href="https://drive.google.com/uc?export=download&id=1IkWhgGQzGkwUm1mD7T80jHmA66OWos9M" download="anniversary">Download</a>
+    </div>
+  );
+}
+
+export default App;
+//https://drive.google.com/drive/folders/1GwtWCxjdEzhOgiunKBIq5EF6NvRZ1T6j?usp=sharing
+//https://drive.google.com/uc?export=download&id=
