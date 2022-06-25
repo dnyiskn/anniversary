83c9f1ce742a0f0e3dce3e65d91b2e619f5d0359
dnyiskn <dnyiskn@gmail.com>
2022-06-24 21:49:44 -0400

    styled page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
-
+import rings from './rings.jpg';
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Happy anniversary</h1>
+        <h1>55</h1>
+        
+        <div>
+          <ul className='instructions'><li><h2>Computer only</h2></li>
+          <li>Click " <em>Download</em> "</li>
+          <li>Click " <em>Download Anway</em> "</li>
+          <li>Wait for Download to begin</li></ul>
+        </div>
+        <button>
+          <a href="https://drive.google.com/uc?export=download&id=1fDiK41VicGfSCYS_eAj_muuGhDvLhgeh" download="anniversary">Download</a>
+        </button>
       </div>
-      <button>
-      <a href="https://drive.google.com/uc?export=download&id=1fDiK41VicGfSCYS_eAj_muuGhDvLhgeh" download="anniversary">Download</a>
-      </button>
+      
     </div>
   );
 }
